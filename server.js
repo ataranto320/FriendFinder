546019589889b4ae5353fb410e32c31eb2c39350
Austin T <austin.taranto@gmail.com>
2019-07-21 16:51:22 -0400

    added server.js code

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+
+// tells node that we are creating an "express" server 
+var app = express();
+
+// sets an initial port
+var PORT = process.env.PORT || 8080;
+
+// sets up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// the below points our server to a series of "route" files
+// these routes give our server a "map" of how to respond when users visit or request data from various URLs.
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// the below code effectively "starts" our server
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
