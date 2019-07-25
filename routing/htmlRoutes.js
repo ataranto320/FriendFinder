33d5157bcdaaf79d183900e7fb02a0d0e8068ae3
Austin T <austin.taranto@gmail.com>
2019-07-24 22:26:24 -0400

    home pg connects to survey og

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -8,11 +8,12 @@ module.exports = function(app) {
   // in each of the below cases the user is shown an html page of content
 
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "Users/austin/Desktop/Trilogy-Hw/FriendFinder/public/survey.html"));
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "Users/austin/Desktop/Trilogy-Hw/FriendFinder/public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
